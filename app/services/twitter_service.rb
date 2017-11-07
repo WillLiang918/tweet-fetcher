326a5bf849db3e831752037946b7296d4fa66ce6
William Liang <williamliang918@gmail.com>
2017-11-07 06:27:41 -0500

    Ingest new tweets

--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -8,4 +8,15 @@ class TwitterService
 
     @client = Twitter::REST::Client.new(config)
   end
+
+  def ingest_new_tweets(user, new_tweets)
+    new_tweets.each do |tweet|
+      UserUnreadTweet.create({
+                                 user_id:         user.id,
+                                 tweet_id:        tweet.id,
+                                 twitter_user_id: tweet.user.id,
+                                 text:            tweet.text
+                             })
+    end
+  end
 end
