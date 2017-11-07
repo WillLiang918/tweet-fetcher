fa2d9da9ba87ab43d9a6acd513b61c67aaf58dfc
William Liang <williamliang918@gmail.com>
2017-11-07 06:30:14 -0500

    Render unread user tweets filtered by twitter user

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,9 +5,39 @@ class TweetsController < ApplicationController
   end
 
   def tweets_by_user
-    client = twitter_service.client
-    fetched_user   = client.user(params[:search])
+    client       = twitter_service.client
+    fetched_user = client.user(params[:search])
+
+    twitter_user = TwitterUser.find_or_create_by(twitter_id: fetched_user.id) do |twitter_user|
+      twitter_user.screen_name = fetched_user.screen_name
+    end
+
+    options = {
+        count: DEFAULT_COUNT,
+    }
+
+    tweets_by_user     = current_user.tweets.for_twitter_user(twitter_user.twitter_id)
+    last_tweet         = tweets_by_user.order('tweet_id').last
+
+    options[:since_id] = last_tweet.tweet_id if last_tweet.present?
+
+    new_tweets = client.user_timeline(twitter_user.screen_name, options)
+
+    @twitter_service.ingest_new_tweets(current_user, new_tweets)
+
+    @tweets = current_user.tweets.unread.for_twitter_user(twitter_user.twitter_id)
   end
+
+  def read
+    tweet = UserUnreadTweet.find(params[:tweet_id])
+    tweet.update_attribute(:read, 1)
+    twitter_user = tweet.twitter_user
+    twitter_user.screen_name
+
+    redirect_to tweets_by_user_path(search: twitter_user.screen_name)
+  end
+
+  private
 
   def twitter_service
     @twitter_service ||= TwitterService.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'tweets#search_twitter_user'
+
+  get 'tweets_by_user', to: 'tweets#tweets_by_user'
+
+  patch 'mark_as_read/:tweet_id', to: 'tweets#read'
 end
